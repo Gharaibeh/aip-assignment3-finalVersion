47c3027f97b885e8e016f7869b0ec70753fa6fb9
Hasan Gharaibeh <swehu1988@gmail.com>
2017-11-11 17:26:33 +1100

    findUser new function (resolve redundancy)

--- ServerSide/controllers/AppointmentController.js
+++ ServerSide/controllers/AppointmentController.js
@@ -28,8 +28,48 @@ exports.add = function (req) {
         return err;
     }
     return User.findOne({_id:req.query.CurrentUserId,Type:UserType.Type[1]}).then(function (currentUser) {
-        if (currentUser) {
-            return User.findOne({Email: req.query.Email, Type: UserType.Type[2],Doctor: currentUser._id}).then(function (findedUser) {
+         if (currentUser) 
+            findUser(req);
+        
+    });
+}
+
+exports.edit = function (req) {
+    if (err = check(req)) {
+        return err;
+    }
+    if (!req.query.Id)
+    {
+        return Promise.resolve({Error:"Parameter (Id) is required"});
+    }
+    return User.findOne({_id:req.query.CurrentUserId,Type:UserType.Type[1]}).then(function (currentUser) {
+        if (currentUser) 
+            findUser(req);
+    })
+}
+
+exports.delete = function (req) {
+    if (!req.query.CurrentUserId)
+    {
+        return Promise.resolve({Error:"Parameter (CurrentUserId) is required"});
+    }
+    if(!req.query.Id) {
+        return Promise.resolve({Error:'Parameter (Id) is required'});
+    }
+    if(Wrong.email(req,true)) {
+        return Wrong.email(req,true);
+    }
+    return User.findOne({_id:req.query.CurrentUserId,Type:UserType.Type[1]}).then(function (currentUser) {
+     
+        if (currentUser) 
+            findUser(req);
+    })
+}
+
+
+function findUser(req){
+    
+             return User.findOne({Email: req.query.Email, Type: UserType.Type[2],Doctor: currentUser._id}).then(function (findedUser) {
                 if (findedUser) {
                     var Appointment = {
                         Date: new Date(req.query.Date), 
@@ -68,107 +108,5 @@ exports.add = function (req) {
         {
             return Promise.resolve({Error: "Doctor with this id not find"});
         }
-    });
-}
-
-exports.edit = function (req) {
-    if (err = check(req)) {
-        return err;
-    }
-    if (!req.query.Id)
-    {
-        return Promise.resolve({Error:"Parameter (Id) is required"});
-    }
-    return User.findOne({_id:req.query.CurrentUserId,Type:UserType.Type[1]}).then(function (currentUser) {
-        if (currentUser) {
-            return User.findOne({Email: req.query.Email, Type: UserType.Type[2],Doctor:currentUser._id}).then(function (findedUser) {
-                if (findedUser) {
-                    var Appointment = {
-                        Date: new Date(req.query.Date), Medication: req.query.Medication,
-                        Log: req.query.Log, Id: req.query.Id
-                    };
-                    var AppointmentWasFind = false;
-                    var TimeIsBusy = false;
-                    for (var i = 0; i < findedUser.Appointments.length; i++) {
-                        if (findedUser.Appointments[i].Id == req.query.Id) {
-                            AppointmentWasFind = true;
-                            findedUser.Appointments[i] = Appointment;
-                        }
-                        else if (findedUser.Appointments[i].Date == Appointment.Date.toString()) {
-                            TimeIsBusy = true;
-                        }
-                    }
-                    if (!AppointmentWasFind) {
-                        return Promise.resolve({Error: "Appointment with this Id not found"});
-                    }
-                    if (TimeIsBusy) {
-                        return Promise.resolve({Error: 'This time is busy by another appointment'});
-                    }
-
-                    return findedUser.save().then(function (changedUser) {
-                        if (changedUser) {
-                            return Promise.resolve({Ok: 'Appointment was changed'});
-                        } else {
-                            return Promise.resolve({Error: "Unknown error"});
-                        }
-                    });
-                }
-                else {
-                    return Promise.resolve({Error: "Available patient with this email not find"});
-                }
-            })
-        }
-        else
-        {
-            return Promise.resolve({Error: "Doctor with this id not find"});
-        }
-    })
-}
-
-exports.delete = function (req) {
-    if (!req.query.CurrentUserId)
-    {
-        return Promise.resolve({Error:"Parameter (CurrentUserId) is required"});
-    }
-    if(!req.query.Id) {
-        return Promise.resolve({Error:'Parameter (Id) is required'});
-    }
-    if(Wrong.email(req,true)) {
-        return Wrong.email(req,true);
-    }
-    return User.findOne({_id:req.query.CurrentUserId,Type:UserType.Type[1]}).then(function (currentUser) {
-        if (currentUser) {
-            return User.findOne({Email: req.query.Email, Type: UserType.Type[2],Doctor:currentUser._id}).then(function (findedUser) {
-                if (findedUser) {
-                    var AppointmentWasFind = false;
-                    var deleteIndex = 0;
-                    for (var i = 0; i < findedUser.Appointments.length; i++) {
-                        if (findedUser.Appointments[i].Id == req.query.Id) {
-                            AppointmentWasFind = true;
-                            deleteIndex = i;
-                        }
-                    }
-                    if (!AppointmentWasFind) {
-                        return Promise.resolve({Error: "Appointment with this Id not found"});
-                    }
-                    findedUser.Appointments.splice(deleteIndex, 1);
-
-                    return findedUser.save().then(function (changedUser) {
-                        if (changedUser) {
-                            return Promise.resolve({Ok: 'Appointment was deleted'});
-                        } else {
-                            return Promise.resolve({Error: "Unknown error"});
-                        }
-                    });
-                }
-                else {
-                    return Promise.resolve({Error: "Available patient with this email not find"});
-                }
-            })
-        }
-        else
-        {
-            return Promise.resolve({Error: "Doctor with this id not find"});
-        }
-    })
+    
 }
